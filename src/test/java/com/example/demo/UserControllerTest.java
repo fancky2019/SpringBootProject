8cfd7d42857fa42fdac5410fefb1db137d4bb3e3
fancky2019 <fancky2019@users.noreply.github.com>
2018-12-21 14:55:08 +0800

    Update UserControllerTest.java

--- src/test/java/com/example/demo/UserControllerTest.java
+++ src/test/java/com/example/demo/UserControllerTest.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.model.entity.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
