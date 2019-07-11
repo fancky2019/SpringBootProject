6abf1f12ae9df530a88ad2086c1015d41eae8d67
fancky <709737588@qq.com>
2019-07-11 11:03:14 +0800

    1

--- src/main/java/com/example/demo/dao/wms/ProductMapper.java
+++ src/main/java/com/example/demo/dao/wms/ProductMapper.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 /*
 Mapper加@Repository注解Service层依赖注入ProductMapper就不会报错红丝波浪线（其实没有错）。
+如果Mapper找不到看：application.yml文件MyBatis是否配置。
+                    Mapper.xml文件包名称，函数名称对应是否对应，Mapper.xml内容是否错误。
  */
 @Repository
 public interface ProductMapper {
