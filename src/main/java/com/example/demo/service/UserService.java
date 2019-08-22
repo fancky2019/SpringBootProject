42b5d415f89112f97b06e91972abba0d2b1cf5cf
fancky <709737588@qq.com>
2019-08-22 14:35:39 +0800

    mybatis  事务

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,9 +2,12 @@ package com.example.demo.service;
 
 import com.example.demo.dao.rabc.UsersMapper;
 import com.example.demo.model.entity.rabc.Users;
+import com.example.demo.model.entity.wms.Order;
+import com.example.demo.model.viewModel.MessageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sun.reflect.generics.tree.VoidDescriptor;
 
 import java.util.concurrent.CompletableFuture;
@@ -65,4 +68,35 @@ public class UserService {
     }
 
 
+    /*
+
+springboot开启事务很简单，只需要一个注解@Transactional 就可以了。
+因为在springboot中已经默认对jpa、jdbc、mybatis开启了事事务，
+引入它们依赖的时候，事物就默认开启
+
+
+
+     自动回滚：  在@Transactional注解中如果不配置rollbackFor属性,那么事物只会在遇到RuntimeException的时候才会回滚,
+    加上rollbackFor=Exception.class,Exception还要抛出。 可以让事物在遇到非运行时异常时也回滚
+
+    手动回滚：TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public MessageResult<Void> transactionTest(Order order) {
+        MessageResult<Void> messageResult = new MessageResult<>();
+
+        //自动回滚：
+        //以下进行数据库事务操作，不要try..catch，以便让spring自动进行事务回滚。
+        // 在controller捕捉异常，判断 messageResult.getSuccess()判断事务是否执行成功，
+        //如果发生异常，抛到controller里，则不成功。
+
+
+
+        //手动回滚:
+        //手动回滚可解决数据库操作没有发生异常的情况，即：可以解决业务方面的异常，可手动控制异常
+        //更加灵活。
+        return  messageResult;
+    }
 }
+
+
---
+++ src/main/java/com/example/demo/service/wms/OrderManagerService.java
@@ -0,0 +1,251 @@
+package com.example.demo.service.wms;
+
+import com.example.demo.dao.wms.OrderDetailMapper;
+import com.example.demo.dao.wms.OrderMapper;
+import com.example.demo.model.entity.wms.Order;
+import com.example.demo.model.entity.wms.OrderDetail;
+import com.example.demo.model.viewModel.MessageResult;
+import com.example.demo.model.viewModel.OrderManagerVM;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+/*
+springboot开启事务很简单，只需要一个注解@Transactional 就可以了。
+因为在springboot中已经默认对jpa、jdbc、mybatis开启了事事务，
+引入它们依赖的时候，事物就默认开启
+ */
+@Service
+public class OrderManagerService {
+
+    @Autowired
+    OrderMapper orderMapper;
+    @Autowired
+    OrderDetailMapper orderDetailMapper;
+
+    /*
+    自动回滚：  在@Transactional注解中如果不配置rollbackFor属性,那么事物只会在遇到RuntimeException的时候才会回滚,
+    加上rollbackFor=Exception.class,Exception还要抛出。 可以让事物在遇到非运行时异常时也回滚
+
+    手动回滚：TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+     */
+    @Transactional
+//    @Transactional(rollbackFor = Exception.class)
+    public MessageResult<Void> addOrder(OrderManagerVM orderManagerVM) {
+        MessageResult<Void> messageResult = new MessageResult<>();
+        try {
+            Order order = new Order();
+            order.setGuid(UUID.randomUUID().toString());
+            order.setOrdernumber(orderManagerVM.getOrderNumber());
+            order.setCreatetime(new Timestamp(System.currentTimeMillis()));
+            order.setOrdertype(1);
+            order.setStatus(Short.parseShort("1"));
+            Integer result = orderMapper.insert(order);
+            if (result <= 0) {
+                messageResult.setMessage("保存失败");
+                messageResult.setSuccess(false);
+                return messageResult;
+            }
+            //获取刚插入的订单，生成的订单ID
+            Integer orderId = order.getId();
+//            orderManagerVM.getProductOrderDetail().forEach(p ->
+            for (OrderDetail p : orderManagerVM.getProductOrderDetail()) {
+                OrderDetail orderDetail = new OrderDetail();
+                orderDetail.setOrderid(orderId);
+                orderDetail.setGuid(UUID.randomUUID().toString());
+                orderDetail.setProductid(p.getProductid());
+                orderDetail.setCount(p.getCount());
+                orderDetail.setDealprice(p.getDealprice());
+                orderDetail.setStatus(Short.parseShort("1"));
+                Integer re = orderDetailMapper.insert(orderDetail);
+                if (re <= 0) {
+                    TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                    messageResult.setMessage("保存失败");
+                    messageResult.setSuccess(false);
+                    return messageResult;
+                }
+            }
+            messageResult.setSuccess(true);
+        } catch (Exception ex) {
+            messageResult.setMessage(ex.getMessage());
+            messageResult.setSuccess(false);
+            //事务回滚
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+
+        } finally {
+            return messageResult;
+        }
+    }
+
+    /*
+    {
+	"orderNumber":"12345678",
+	"productOrderDetail":[
+		{
+			"productid":2115,
+			"count":30,
+			"dealprice":25.5
+		},
+			{
+			"productid":2114,
+			"count":208,
+			"dealprice":20
+		}
+		]
+
+}
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public MessageResult<Void> addOrderAndOrderDetails(OrderManagerVM orderManagerVM) throws Exception{
+        MessageResult<Void> messageResult = new MessageResult<>();
+
+        Order order = new Order();
+        order.setGuid(UUID.randomUUID().toString());
+        order.setOrdernumber(orderManagerVM.getOrderNumber());
+        order.setCreatetime(new Timestamp(System.currentTimeMillis()));
+        order.setOrdertype(1);
+        order.setStatus(Short.parseShort("1"));
+        Integer result = orderMapper.insert(order);
+        if (result <= 0) {
+            throw  new Exception("保存失败");
+//            messageResult.setMessage("保存失败");
+//            messageResult.setSuccess(false);
+//
+//            return messageResult;
+        }
+        //获取刚插入的订单，生成的订单ID
+        Integer orderId = order.getId();
+//            orderManagerVM.getProductOrderDetail().forEach(p ->
+        for (OrderDetail p : orderManagerVM.getProductOrderDetail()) {
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrderid(orderId);
+            orderDetail.setGuid(UUID.randomUUID().toString());
+            orderDetail.setProductid(p.getProductid());
+            orderDetail.setCount(p.getCount());
+            orderDetail.setDealprice(p.getDealprice());
+            orderDetail.setStatus(Short.parseShort("1"));
+
+
+          //  Integer m=Integer.valueOf("m");
+
+            Integer re = orderDetailMapper.insert(orderDetail);
+            if (re <= 0) {
+                throw  new Exception("保存失败");
+//                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+//                messageResult.setMessage("保存失败");
+//                messageResult.setSuccess(false);
+//                return messageResult;
+            }
+        }
+        messageResult.setSuccess(true);
+        return messageResult;
+    }
+
+    /*
+     自动回滚在try...catch中没有继续抛出异常不能自动回滚
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public MessageResult<Void> addOrderAndOrderDetailsTry(OrderManagerVM orderManagerVM) throws  Exception {
+
+
+        MessageResult<Void> messageResult = new MessageResult<>();
+        try {
+
+            Order order = new Order();
+            order.setGuid(UUID.randomUUID().toString());
+            order.setOrdernumber(orderManagerVM.getOrderNumber());
+            order.setCreatetime(new Timestamp(System.currentTimeMillis()));
+            order.setOrdertype(1);
+            order.setStatus(Short.parseShort("1"));
+            Integer result = orderMapper.insert(order);
+            if (result <= 0) {
+
+                messageResult.setMessage("保存失败");
+                messageResult.setSuccess(false);
+
+            } else {
+                messageResult.setSuccess(true);
+            }
+            //获取刚插入的订单，生成的订单ID
+            Integer orderId = order.getId();
+//            orderManagerVM.getProductOrderDetail().forEach(p ->
+            for (OrderDetail p : orderManagerVM.getProductOrderDetail()) {
+                OrderDetail orderDetail = new OrderDetail();
+                orderDetail.setOrderid(orderId);
+                orderDetail.setGuid(UUID.randomUUID().toString());
+                orderDetail.setProductid(p.getProductid());
+                orderDetail.setCount(p.getCount());
+                orderDetail.setDealprice(p.getDealprice());
+                orderDetail.setStatus(Short.parseShort("1"));
+
+
+                Integer m = Integer.valueOf("m");
+
+                Integer re = orderDetailMapper.insert(orderDetail);
+                if (re <= 0) {
+                    messageResult.setMessage("保存失败");
+                    messageResult.setSuccess(false);
+
+                } else {
+                    messageResult.setSuccess(true);
+                }
+            }
+        } catch (Exception ex) {
+            messageResult.setMessage(ex.getMessage());
+            messageResult.setSuccess(false);
+
+            //如果不抛出异常，将不能自动回滚
+            throw  ex;
+        }
+
+
+        return messageResult;
+    }
+
+    /*
+    自动回滚
+    @Transactional默认只回滚RunTimeException级别，
+    如果需要回滚到Exception级别才需要指定@Transactional(rollbackFor=Exception.class) ，Exception还要抛出。
+    表示Exception级别及一下均会回滚
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public MessageResult<Void> deleteOrder(Order order) {
+        MessageResult<Void> messageResult = new MessageResult<>();
+        try {
+//                OrderDetail orderDetail=new OrderDetail();
+//                orderDetail.setOrderid(order.getId());
+//                List<OrderDetail> orderDetailList=orderDetailMapper.select(orderDetail);
+            Integer result = orderMapper.deleteByPrimaryKey(order.getId());
+            if (result <= 0) {
+                messageResult.setMessage("删除失败");
+                messageResult.setSuccess(false);
+                return messageResult;
+            }
+            Integer re = orderDetailMapper.deleteByOrderId(order);
+            if (re <= 0) {
+                TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+                messageResult.setMessage("删除失败");
+                messageResult.setSuccess(false);
+                return messageResult;
+            }
+            messageResult.setSuccess(true);
+        } catch (Exception ex) {
+            messageResult.setMessage(ex.getMessage());
+            messageResult.setSuccess(false);
+            //事务回滚 手动回滚
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+
+            //或者
+//            throw  new  Exception(ex.getMessage());
+//            throw ex;
+        } finally {
+            return messageResult;
+        }
+    }
+
+
+}
