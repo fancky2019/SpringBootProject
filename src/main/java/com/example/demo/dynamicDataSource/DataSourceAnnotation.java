9a70e1aa5d8f7d7c7c6d6c2387ff7c1eaa4d6f38
fancky <709737588@qq.com>
2020-04-08 15:50:38 +0800

    11

--- src/main/java/com/example/demo/dynamicDataSource/DataSourceAnnotation.java
+++ src/main/java/com/example/demo/dynamicDataSource/DataSourceAnnotation.java
@@ -1,12 +1,12 @@
-//package com.example.demo.dynamicDataSource;
-//
-//
-//import java.lang.annotation.*;
-//
-//@Documented
-//@Inherited
-//@Retention(RetentionPolicy.RUNTIME)
-//@Target({ElementType.METHOD, ElementType.TYPE})
-//public @interface DataSourceAnnotation {
-//     String value() default DataSourceStrings.WRITER;
-//}
+package com.example.demo.dynamicDataSource;
+
+
+import java.lang.annotation.*;
+
+@Documented
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface DataSourceAnnotation {
+     String value() default DataSourceStrings.WRITER;
+}
