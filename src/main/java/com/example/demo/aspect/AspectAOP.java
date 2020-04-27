735106bfc2c55db371a56d1473c3234697739e04
fancky <709737588@qq.com>
2020-04-27 17:54:39 +0800

    1

---
+++ src/main/java/com/example/demo/aspect/AspectAOP.java
@@ -0,0 +1,13 @@
+package com.example.demo.aspect;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/*
+AOP面向切面：
+实现
+一、JDK动态代理的两个核心接口(类)分别是InvocationHandler和Proxy。注意：只能代理接口。
+    代码见Demo2019--Test2019--proxy目录下
+二、CGLIB动态代理模拟
+ */
+
