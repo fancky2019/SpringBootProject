feb9f216467987517987a7e5b7b56bd7e117d03a
fancky2019 <709737588@qq.com>
2021-03-19 16:00:10 +0800

    多个实现类注入、构造器注入

---
+++ src/main/java/com/example/demo/service/test/InterfaceTestImpA.java
@@ -0,0 +1,21 @@
+package com.example.demo.service.test;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+
+/*
+ *建议用@Service加别名
+ *如果类用@Component做注解而不是@Service，就不用了指定别名。
+ * 如果用@Service注解，多个类实现接口时候就要指定别名：声明@Service("InterfaceTestImpB")，调用 @Autowired、 @Qualifier("InterfaceTestImpA")
+ */
+//@Service
+@Service("InterfaceTestImpA")
+//@Component
+public class InterfaceTestImpA implements InterfaceTest{
+
+    @Override
+    public String fun() {
+        return "InterfaceTestImpA";
+    }
+}
