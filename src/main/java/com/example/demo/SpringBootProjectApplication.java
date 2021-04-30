c924e9a8ef550825331133759c09a5606c9cf310
fancky2019 <709737588@qq.com>
2021-04-30 14:29:40 +0800

    整理

--- src/main/java/com/example/demo/SpringBootProjectApplication.java
+++ src/main/java/com/example/demo/SpringBootProjectApplication.java
@@ -1,20 +1,16 @@
 package com.example.demo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 //默认扫描的是启动类所在的包及其子包
 //由于采用多数据源，禁用springboot默认的数据源配置，多数据源不适合微服务设计理念废弃。采用分布式事务。
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
@@ -40,9 +36,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //移动到SwaggerConfig 配置文件类处理
 //@EnableSwagger2  //EnableSwagger2  http://localhost:8081/swagger-ui.html
-public class DemoApplication {
+public class SpringBootProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(SpringBootProjectApplication.class, args);
     }
 }
