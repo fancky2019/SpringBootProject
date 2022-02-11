d0dcea3be6ecc64d5f7a778f6d580442491a9450
fancky <709737588@qq.com>
2022-02-11 19:04:04 +0800

    1

--- src/main/java/com/example/demo/model/pojo/BeanLife.java
+++ src/main/java/com/example/demo/model/pojo/BeanLife.java
@@ -1,2 +1,55 @@
-package com.example.demo.model.pojo;public class BeanLife {
+package com.example.demo.model.pojo;
+
+import com.example.demo.controller.UtilityController;
+import lombok.Data;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 测试方法在UtilityControllerTest
+ */
+
+//public class BeanLife {
+public class BeanLife implements InitializingBean, DisposableBean {
+    //Lombox 的注解 @Slf4j 相当于下面语句
+    private static final Logger logger = LogManager.getLogger(BeanLife.class);
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        logger.info("BeanLife set property");
+    }
+
+    /**
+     * 指定指定initMethod和destroyMethod的执行顺序：BeanLife constructor-->BeanLife init-->BeanLife destroy
+     */
+    public BeanLife() {
+        logger.info("BeanLife constructor");
+    }
+
+    //  @Bean(initMethod = "init", destroyMethod = "destroy")
+    public void init() {
+        logger.info("BeanLife init");
+    }
+
+    public void destroy() {
+        logger.info("BeanLife destroy");
+    }
+
+    /*
+    通过实现接口InitializingBean, DisposableBean
+    执行顺序：constructor-->afterPropertiesSet--> destroy
+     */
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        logger.info("BeanLife afterPropertiesSet");
+    }
 }
