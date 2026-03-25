997a10cfc3121198b16a00c2dca4aab135f8fe64
fancky2019 <709737588@qq.com>
2026-03-25 15:51:46 +0800

    sleuth traceId

--- src/main/java/com/example/demo/utility/TraceIdCreater.java
+++ src/main/java/com/example/demo/utility/TraceIdCreater.java
@@ -7,6 +7,41 @@ import brave.internal.RecyclableBuffers;
  * 用到了io.zipkin.brave包下的两个类
  *
  * 可以引入sleuth 依赖
+ *
+ *
+ * // 下游服务接收到请求时
+ * public class TracingFilter {
+ *     public void doFilter(ServletRequest request, ...) {
+ *         HttpServletRequest httpRequest = (HttpServletRequest) request;
+ *
+ *         // 从请求头中提取上游传递的 traceId
+ *         String traceId = httpRequest.getHeader("X-B3-TraceId");
+ *         String parentSpanId = httpRequest.getHeader("X-B3-SpanId");
+ *
+ *         if (traceId != null) {
+ *             // 使用传递过来的 traceId，生成新的 spanId
+ *             MDC.put("traceId", traceId);
+ *             MDC.put("spanId", generateSpanId());
+ *             MDC.put("parentSpanId", parentSpanId);
+ *         }
+ *     }
+ * }
+ *
+ * 传递的 HTTP 头（B3 协议）
+ * Sleuth 使用 B3 协议（Zipkin 的标准）在服务间传递追踪信息：
+ *
+ * Header 名称	             说明	                 示例
+ * X-B3-TraceId	            全局唯一的链路 ID	           a1b2c3d4e5f67890
+ * X-B3-SpanId	            当前 Span 的 ID	           1234567890abcdef
+ * X-B3-ParentSpanId	    父 Span 的 ID	            fedcba0987654321
+ * X-B3-Sampled	            是否采样（1=采样，0=不采样）	1
+ *
+ *
+ * 消息队列（MQ）传递 TraceId：消息头中自动包含 X-B3-TraceId
+ *
+ *
+ *
+ *
  */
 public class TraceIdCreater {
 
